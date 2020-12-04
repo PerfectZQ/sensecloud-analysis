d6904a1a1e75a3ce372fb5e55e99d73de906d707
zhangqiang <zhangqiang@sensetime.com>
2020-12-04 17:50:36 +0800

    添加新表的DDL脚本

---
+++ web/db_init/user.sql
@@ -0,0 +1,10 @@
+-- sensecloud.`user` definition
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_UN` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
