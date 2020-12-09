b4a5058519892f9dd648a0f4bc328b73a3e0c85a
lijingyu2 <lijingyu2@sensetime.com>
2020-12-09 11:02:58 +0800

    fix reflection bug

--- web/src/main/java/sensecloud/web/bean/ConnectorBean.java
+++ web/src/main/java/sensecloud/web/bean/ConnectorBean.java
@@ -3,6 +3,7 @@ package sensecloud.web.bean;
 import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.extension.handlers.FastjsonTypeHandler;
 import lombok.Data;
 import sensecloud.web.config.db.JSONObjectHandler;
 
@@ -18,12 +19,12 @@ public class ConnectorBean implements Serializable {
     private String sourceName;
     private String sourceType;
 
-    @TableField(typeHandler = JSONObjectHandler.class)
+    @TableField(typeHandler = FastjsonTypeHandler.class)
     private JSONObject sourceConf;
     private String sinkName;
     private String sinkType;
 
-    @TableField(typeHandler = JSONObjectHandler.class)
+    @TableField(typeHandler = FastjsonTypeHandler.class)
     private JSONObject sinkConf;
 
     private String createBy;
