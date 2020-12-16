f743a24732c0c2368af80c896293cf65b306519c
zhangqiang <zhangqiang@sensetime.com>
2020-12-16 16:06:36 +0800

    Add token authentication

--- web/src/main/java/sensecloud/web/filter/TokenAuthenticationFilter.java
+++ web/src/main/java/sensecloud/web/filter/TokenAuthenticationFilter.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
