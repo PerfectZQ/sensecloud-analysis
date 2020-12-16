404255fe0bc31a3c533943b9974614c3ce5ef41c
zhangqiang <zhangqiang@sensetime.com>
2020-12-16 16:22:05 +0800

    Add token authentication

--- web/src/main/java/sensecloud/web/config/security/WebSecurityConfig.java
+++ web/src/main/java/sensecloud/web/config/security/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.expression.SecurityExpressionOperations;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -71,13 +72,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        tokenAuthenticationFilter.setAuthenticationManager(http.getSharedObject(AuthenticationManager.class));
+        // tokenAuthenticationFilter.setAuthenticationSuccessHandler();
+        // tokenAuthenticationFilter.setAuthenticationFailureHandler();
         http.addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .antMatchers("/login/**", "/logout/**")
                 .permitAll() // Specify that URLs are allowed by anyone(includes anonymous user).
                 .anyRequest()
                 .authenticated() // Specify that URLs are allowed by any authenticated user.
-            .and()
+                .and()
                 /*
                 .formLogin()
                 .loginPage("/login") // Indicate login URL
@@ -90,7 +94,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
                  */
                 .httpBasic()
-            .and()
+                .and()
                 .csrf()
                 .disable();
     }
