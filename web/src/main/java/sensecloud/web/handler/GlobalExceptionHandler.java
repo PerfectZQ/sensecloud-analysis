0cb41829cc28b38ca02832682cac7cf174f37172
lijingyu2 <lijingyu2@sensetime.com>
2020-12-28 13:16:45 +0800

    test 401

--- web/src/main/java/sensecloud/web/handler/GlobalExceptionHandler.java
+++ web/src/main/java/sensecloud/web/handler/GlobalExceptionHandler.java
@@ -3,10 +3,12 @@ package sensecloud.web.handler;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import sensecloud.auth2.config.SSOConfiguration;
 import sensecloud.web.bean.vo.ResultVO;
@@ -33,7 +35,7 @@ public class GlobalExceptionHandler {
      * @return
      */
     @ExceptionHandler(value = AuthenticationException.class)
-    // @ResponseStatus(code = HttpStatus.UNAUTHORIZED, reason = "AuthenticationException")
+     @ResponseStatus(code = HttpStatus.UNAUTHORIZED, reason = "AuthenticationException")
     public ResultVO<Object> exceptionHandler(HttpServletRequest req, HttpServletResponse response, AuthenticationException e) throws IOException {
         log.error("====> GlobalExceptionHandler AuthenticationException: ", e);
         // response.sendRedirect(this.configuration.getNo_auth_redirect_url());
