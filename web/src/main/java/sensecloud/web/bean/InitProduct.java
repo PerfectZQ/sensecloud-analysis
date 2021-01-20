584b6ca5d29e153772502e296c7bc0bcb03173ea
zhangqiang <zhangqiang@sensetime.com>
2021-01-20 14:59:56 +0800

    add validators

--- web/src/main/java/sensecloud/web/bean/InitProduct.java
+++ web/src/main/java/sensecloud/web/bean/InitProduct.java
@@ -4,6 +4,9 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
+import org.springframework.util.StringUtils;
+
+import javax.validation.constraints.NotBlank;
 
 /**
  * @author zhangqiang
@@ -13,12 +16,24 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 @ApiModel(value = "InitGroup", description = "初始化产品线")
 public class InitProduct {
+
+    @NotBlank
     @ApiModelProperty("产品线管理员的用户名")
     private String username;
+    @NotBlank
     @ApiModelProperty("产品线服务名称")
     private String productName;
     @ApiModelProperty("产品线对应 Gitlab 库地址")
     private String repository;
     @ApiModelProperty("产品线对应 Gitlab 库分支")
     private String branch;
+
+    /**
+     * 判断是否包含合法的 Git 信息
+     *
+     * @return
+     */
+    public Boolean isGitEmpty() {
+        return StringUtils.isEmpty(repository) || StringUtils.isEmpty(branch);
+    }
 }
