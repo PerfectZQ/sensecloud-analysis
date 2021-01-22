9b615f30cf271b9a3a8a2fa802bd2d233c78255e
lijingyu2 <lijingyu2@sensetime.com>
2021-01-22 18:24:45 +0800

    add StartupProcessor

---
+++ flow-manager/src/main/java/sensecloud/flow/StartupProcessor.java
@@ -0,0 +1,24 @@
+package sensecloud.flow;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import sensecloud.flow.generator.DAGDefinitionProvider;
+
+@Slf4j
+@Order(99)
+@Component
+public class StartupProcessor implements CommandLineRunner {
+
+    @Autowired
+    private DAGDefinitionProvider provider;
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info(">>> Flow manager setup start ...");
+        provider.loadFromClassPath();
+        log.info(">>> Flow manager setup end!");
+    }
+}
