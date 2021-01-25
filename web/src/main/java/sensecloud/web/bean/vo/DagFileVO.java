f6ef153622329820fe6e37dca87de586425ee31e
lijingyu2 <lijingyu2@sensetime.com>
2021-01-25 10:01:32 +0800

    test

--- web/src/main/java/sensecloud/web/bean/vo/DagFileVO.java
+++ web/src/main/java/sensecloud/web/bean/vo/DagFileVO.java
@@ -2,11 +2,13 @@ package sensecloud.web.bean.vo;
 
 import lombok.Data;
 
+import java.math.BigInteger;
+
 @Data
 public class DagFileVO {
 
     private String fileLoc;
-    private long fileLocHash;
+    private BigInteger fileLocHash;
     private String fileName;
     private String groupName;
     private String sourceCode;
